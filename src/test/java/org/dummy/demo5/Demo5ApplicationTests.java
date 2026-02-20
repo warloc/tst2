02247fef65e1ef91443d59352ebbcb4ef8ccc5fb
vmerzl110 <Vadym_Merzlikin@comcast.com>
2026-02-20 02:23:02 -0700

    init

--- src/test/java/org/dummy/demo5/Demo5ApplicationTests.java
+++ src/test/java/org/dummy/demo5/Demo5ApplicationTests.java
@@ -9,8 +9,15 @@ class Demo5ApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("DAS_TEST");
-        String s = null;
-        s.length();
+//        String s = null;
+//        s.length();
+        try {
+            System.out.println("DAS_TEST_wait");
+            Thread.sleep(3*60*1000);
+            System.out.println("DAS_TEST_done");
+        } catch (InterruptedException e) {
+        }
+
     }
 
 }
